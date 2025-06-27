7affb17107fe083e71fe56d816667859e594a068
mallikarjuna <mallikarjuna.r@promantia.com>
2025-06-27 15:34:21 +0530

    CI Test

--- test/testing.py
+++ test/testing.py
@@ -1,14 +1,26 @@
+import unittest
 import frappe
 
-frappe.msggprint("This should fail immediately")  
+# ===== MODULE-LEVEL ERRORS =====
+# These will fail during import
+from frappe import non_existent_import  # Will fail
+undefined_variable_at_top = non_existent_global  # Will fail
 
 class TestErrorDetection(unittest.TestCase):
-    def test_errors(self):
-        """Now all errors will execute"""
-        frappe.msgprnt("Misspelled")  
-        frappe.Msgprint("Wrong case") 
-        non_existent_module.error()  
-        
-    def test_valid(self):
-        frappe.msgprint("Correct usage")
-        self.assertTrue(True)
+    def test_attribute_errors(self):
+        """Test undefined attributes/methods"""
+        frappe.non_existent_method()  # Will fail
+        self.non_existent_assert()  # Will fail
+
+    def test_name_errors(self):
+        """Test undefined variables"""
+        print(undefined_variable)  # Will fail
+
+    def test_syntax_errors(self):
+        """Test syntax issues"""
+        return (1 + 2  # Missing closing parenthesis
+
+# ===== FORCE TEST EXECUTION =====
+if __name__ == "__main__":
+    print("=== Running tests directly ===")
+    unittest.main(failfast=True)
